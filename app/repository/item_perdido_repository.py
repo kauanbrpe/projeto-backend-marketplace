ab05c0039defc1aef4ca22be017ac3f6264055fc
JeffersonCVeiga <jeffersonveiga99@gmail.com>
2026-05-27 11:38:50 -0300

    commit do item_perdido_repository e order_repository

---
+++ app/repository/item_perdido_repository.py
@@ -0,0 +1,47 @@
+from app.models.item_perdido_model import ItemPerdidoModel
+from app import db
+
+
+class ItemPerdidoRepository:
+
+    @staticmethod
+    def criar_item(data):
+        novo_item = ItemPerdidoModel(
+            nome_item=data['nome_item'],
+            descricao=data['descricao'],
+            local_perdido=data['local_perdido'],
+            data_perdido=data['data_perdido'],
+            perdido_id=data['perdido_id'],
+            usuario_id=data['usuario_id']
+        )
+
+        db.session.add(novo_item)
+        db.session.commit()
+
+        return novo_item
+
+    @staticmethod
+    def listar_itens():
+        return ItemPerdidoModel.query.all()
+
+    @staticmethod
+    def buscar_por_id(item_id):
+        return ItemPerdidoModel.query.get(item_id)
+
+    @staticmethod
+    def atualizar_item(item, data):
+        item.nome_item = data.get('nome_item', item.nome_item)
+        item.descricao = data.get('descricao', item.descricao)
+        item.local_perdido = data.get('local_perdido', item.local_perdido)
+        item.data_perdido = data.get('data_perdido', item.data_perdido)
+        item.perdido_id = data.get('perdido_id', item.perdido_id)
+        item.usuario_id = data.get('usuario_id', item.usuario_id)
+
+        db.session.commit()
+
+        return item
+
+    @staticmethod
+    def deletar_item(item):
+        db.session.delete(item)
+        db.session.commit()
---
+++ app/repository/order_repository.py
@@ -0,0 +1,58 @@
+from app.models.order_model import OrderModel, OrderItemModel
+from app import db
+
+
+class OrderRepository:
+
+    @staticmethod
+    def criar_pedido(data):
+        novo_pedido = OrderModel(
+            user_id=data['user_id'],
+            status=data.get('status', 'pending'),
+            total_price=data.get('total_price', 0.00),
+            coupon_id=data.get('coupon_id')
+        )
+
+        db.session.add(novo_pedido)
+        db.session.flush()
+
+        itens = data.get('items', [])
+
+        for item in itens:
+            novo_item = OrderItemModel(
+                order_id=novo_pedido.id,
+                product_id=item['product_id'],
+                quantity=item['quantity'],
+                historic_price=item['historic_price']
+            )
+
+            db.session.add(novo_item)
+
+        db.session.commit()
+
+        return novo_pedido
+
+    @staticmethod
+    def listar_pedidos():
+        return OrderModel.query.all()
+
+    @staticmethod
+    def buscar_por_id(order_id):
+        return OrderModel.query.get(order_id)
+
+    @staticmethod
+    def listar_por_usuario(user_id):
+        return OrderModel.query.filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def atualizar_status(order, novo_status):
+        order.status = novo_status
+
+        db.session.commit()
+
+        return order
+
+    @staticmethod
+    def deletar_pedido(order):
+        db.session.delete(order)
+        db.session.commit()
